30301daa10c5505bf0336146760fb194e430f8f9
rumor1993 <rumor1993@naver.com>
2024-04-11 21:35:57 +0900

    feat: redirect 테스트

--- src/main/java/com/rumor/kitchen/config/WebConfig.java
+++ src/main/java/com/rumor/kitchen/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true) // 이 부분 추가
                 .allowedHeaders("*");
