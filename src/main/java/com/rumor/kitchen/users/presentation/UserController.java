0a1b89447fc960e9e0f0e06698cc560facbcecb4
rumor1993 <rumor1993@naver.com>
2024-04-12 01:20:00 +0900

    feat: token

--- src/main/java/com/rumor/kitchen/users/presentation/UserController.java
+++ src/main/java/com/rumor/kitchen/users/presentation/UserController.java
@@ -2,7 +2,6 @@ package com.rumor.kitchen.users.presentation;
 
 import com.rumor.kitchen.users.application.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
