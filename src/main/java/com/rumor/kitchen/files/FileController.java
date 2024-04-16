8c08440b8ba6d601cb4f7aae44ec9ef7334b66de
rumor1993 <rumor1993@naver.com>
2024-04-17 02:28:45 +0900

    feat: Board 수정

---
+++ src/main/java/com/rumor/kitchen/files/FileController.java
@@ -0,0 +1,26 @@
+package com.rumor.kitchen.files;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+@RestController
+@RequestMapping("/files")
+@RequiredArgsConstructor
+public class FileController {
+    private final FileService fileService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> upload(@RequestPart MultipartFile file) throws IOException {
+        return ResponseEntity.ok().body(fileService.saveAndGetPath(file).toString());
+    }
+}
---
+++ src/main/java/com/rumor/kitchen/files/FileService.java
@@ -0,0 +1,28 @@
+package com.rumor.kitchen.files;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class FileService {
+    public Path saveAndGetPath(MultipartFile file) throws IOException {
+        Path directory = Paths.get("data", "kitchen");
+        Path directoryAbsolute = directory.toAbsolutePath().normalize();
+        Files.createDirectories(directory);
+
+        String fileExtension = com.google.common.io.Files.getFileExtension(file.getOriginalFilename());
+        String fileName = UUID.randomUUID() + "." + fileExtension;
+        Path targetPath = directoryAbsolute.resolve(fileName).normalize();
+
+        file.transferTo(targetPath);
+        return directory.resolve(fileName);
+    }
+}
