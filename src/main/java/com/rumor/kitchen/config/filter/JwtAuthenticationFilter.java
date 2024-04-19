7f610dfa856eee83e4f9fd27226a737f1076f6d6
rumor1993 <rumor1993@naver.com>
2024-04-20 01:28:39 +0900

    1차 마무리

--- src/main/java/com/rumor/kitchen/config/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/rumor/kitchen/config/filter/JwtAuthenticationFilter.java
@@ -36,7 +36,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return true;
         }
 
-        String[] excludePath = {"/oauth2/", "/favicon.ico", "/boards", "/swagger-ui"};
+        String[] excludePath = {"/oauth2/", "/favicon.ico", "/boards", "/swagger-ui/", "/v3/"};
         String path = request.getRequestURI();
         return Arrays.stream(excludePath).anyMatch(path::startsWith);
     }
