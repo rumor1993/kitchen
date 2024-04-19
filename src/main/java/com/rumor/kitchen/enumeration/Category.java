3ace7049b87fb658f686835432bdea13a98dad6a
rumor1993 <rumor1993@naver.com>
2024-04-19 13:34:44 +0900

    feat: Category 추가

--- src/main/java/com/rumor/kitchen/enumeration/Category.java
+++ src/main/java/com/rumor/kitchen/enumeration/Category.java
@@ -3,7 +3,7 @@ package com.rumor.kitchen.enumeration;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum Category {
-    KOREAN, CHINESE, JAPANESE, WESTERN;
+    KOREAN, CHINESE, JAPANESE, WESTERN, ARTICLE, POST;
 
     @JsonCreator
     public static Category fromString(String key) {
